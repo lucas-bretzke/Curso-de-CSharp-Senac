cf44c48afbc61802256d2b61051b15cd4d59e745
LUCAS BRETZKE <lucas.bretzke@alunos.sc.senac.br>
2024-10-02 19:35:06 -0300

    arquivo desnecessario deletado

--- ListaDeEcercicios02/Program.cs
+++ ListaDeEcercicios02/Program.cs
@@ -5,7 +5,22 @@ class Program
 {
     static void Main()
     {
-        //Operadores();
+        NumeroImparOuPar();
     }
 
+
+    static void NumeroImparOuPar()
+    {
+        Console.WriteLine("Digite um numero: "); int numero = int.Parse(Console.ReadLine());
+
+
+        //if (numero % 2 == 0)
+        //{
+        //    Console.WriteLine("Número é Par")
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Número é Impar")
+        //}
+    }
 }
